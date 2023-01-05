8dfc38dfd59fc7fd54dd9869a0fb2de5bf8dc7cb
Cian Eastwood <s1668298@talorc.inf.ed.ac.uk>
2023-01-05 17:16:17 +0000

    More cleaning...

--- utils/copy_to_informatics_webpage.sh
+++ utils/copy_to_informatics_webpage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copy all files from repo to informatics personal webpage directory
-rsync -r --exclude '.git' /afs/inf.ed.ac.uk/user/s16/s1668298/cianeastwood.github.io/ /public/homepages/s1668298/web
+rsync -r --exclude '.git' /afs/inf.ed.ac.uk/user/s16/s1668298/cianeastwood.github.io/ /public/homepages/s1668298/web --delete
 
 # Change permissons so files can be served
 find /public/homepages/s1668298/web -type d -exec chmod 755 {} \;
